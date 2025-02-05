9bc00ee67f6b662973944e01ae7829efcd8be4b1
Bree Swn <suwanan.cs@hotmail.com>
2025-02-05 13:01:51 +0700

    add login form component

---
+++ src/components/Input.jsx
@@ -0,0 +1,17 @@
+export default function Input({ placeholder, type = "text", error }) {
+  return (
+    <div>
+      <input
+        type={type}
+        placeholder={placeholder}
+        error={error}
+        className={`w-full px-3 py-1.5 border rounded-md focus:outline-none focus:ring-2 ${
+          error
+            ? "border-red-500 focus:ring-red-300"
+            : "border-gray-300 focus:border-blue-500 focus:ring-blue-300"
+        }`}
+      />
+      {error? <small className="text-red-500">{error}</small> : null }
+    </div>
+  );
+}
